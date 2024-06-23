3a17a27a7887c5d556474f19e5b1d4ceeefcd3c7
RMosca-dev <rodolphe.mosca@gmail.com>
2024-06-23 23:43:34 +0200

    pic5

--- P1/P1C4/énoncé/main.py
+++ P1/P1C4/énoncé/main.py
@@ -1,1 +1,7 @@
 # Écrivez votre code ici !
+nom="Rodolphe Mosca"
+age=55
+print(F"Je m'appelle {nom} et j'ai {age} ans")
+age=age+10
+print(F"Je m'appelle {nom} et j'ai {age} ans maintenant")
+
--- P1/P1C5/énoncé/main.py
+++ P1/P1C5/énoncé/main.py
@@ -1,1 +1,17 @@
 # Écrivez votre code ici !
+nom="Rodolphe Mosca"
+age=55
+taille=1.83
+est_etudiant=False
+
+print(f"nom:{nom}")
+print(f"age:{age}")
+print(f"taille:{taille}")
+print(f"est etudiant:{est_etudiant}")
+
+# Affichage de la nature des variables
+print(f"type nom:{type(nom)}")
+print(f"type age:{type(age)}")
+print(f"type taille:{type(taille)}")
+print(f"type est_etudiant:{type(est_etudiant)}")
+
